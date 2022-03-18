ad99b65f72fffb3f1444502cf5dbabd457ab50d3
Pablo Maldonado <public.maldonado@gmail.com>
2022-03-18 14:11:47 +0000

    feat: add set commands

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,35 @@
 import { Context, Telegraf } from "telegraf";
 import { Update } from "typegram";
 import { getTimezoneTime, timeLeftInDublin } from "./utils";
+import { Telegram } from "telegraf";
+import "dotenv/config";
 
-const bot: Telegraf<Context<Update>> = new Telegraf(
-  process.env.BOT_TOKEN as string
-);
+const BOT_TOKEN = process.env["BOT_TOKEN"];
+
+if (!BOT_TOKEN) {
+  throw new Error("BOT_TOKEN must be defined");
+}
+
+const telegram = new Telegram(BOT_TOKEN);
+telegram.setMyCommands([
+  {
+    command: "pablitoindublin",
+    description: "Diferencia horaria entre Dublin y Montevideo",
+  },
+  {
+    command: "pablitoinmontevideo",
+    description: "En cuántos días vuelve Pablito a Montevideo :)",
+  },
+]);
+
+const bot: Telegraf<Context<Update>> = new Telegraf(BOT_TOKEN);
+
+bot.start((ctx) => {
+  ctx.reply(`Usando los siguientes comandos podes ver:
+
+/pablitoindublin - la diferencia horaria de dublin y montevideo
+/pablitoinmontevideo - en cuántos días vuelve pablito a montevideo :)`);
+});
 
 bot.command(["pablitoindublin"], (ctx) => {
   return ctx.reply(
